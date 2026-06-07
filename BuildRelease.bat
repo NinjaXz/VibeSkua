ce88d01a37c0b9d6d4a0f4407088e596c17ef414
NinjaXz <Ninja1337x@protonmail.com>
2026-06-07 11:42:45 +0400

    Build file update

---
+++ BuildRelease.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Building VibeSkua Release (this will take a moment)...
+
+if exist "Build" rmdir /s /q "Build"
+
+dotnet build Skua.sln -c Release
+
+echo =========================================
+echo Build Complete!
+echo You can find your build inside
+echo the newly created Build folder!
+echo =========================================
+pause
